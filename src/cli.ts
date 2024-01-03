16975aa9da2cfdb9c7440877119a5ba253b703b6
jerbear4328 <iamjerbear4328@gmail.com>
2024-01-03 15:41:54 -0800

    remove Node's warning about experimental features (fetch)

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,7 @@
 import { cli } from './parse.ts'
 
-// process.removeAllListeners('warning')
+// remove Node's warning about experimental features (fetch)
+import process from 'node:process'
+process.removeAllListeners('warning')
 
 await cli(Deno.args)
